4051d88e6e7d99734a17df3e7876da11133ae9fa
Luc Patiny <luc@patiny.com>
2025-04-29 07:21:28 +0200

    wip: use original references values and calculate on the fly

---
+++ data/README.md
@@ -0,0 +1,1 @@
+Data coming from: https://www.kherb.io/docs/nmr_table.html
---
+++ data/parse.mjs
@@ -0,0 +1,25 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+const data = readFileSync(
+  join(import.meta.dirname, 'nmr_table_202504.csv'),
+  'utf8',
+);
+
+const lines = data.split(/\r?\n/).filter(Boolean);
+const headers = lines.shift().split('\t');
+
+const results = {};
+for (const line of lines) {
+  const fields = line.split('\t');
+  const nucleus = fields[1] + fields[2].replace(/^[DT]/, 'H');
+  const gyromagneticRatio = Number(fields[6].replace(/\(.*/, ''));
+  results[nucleus] = gyromagneticRatio;
+}
+
+writeFileSync(
+  join(import.meta.dirname, '../src/gyromagneticRatioMHzT.ts'),
+  `export const gyromagneticRatioMHzT = ${JSON.stringify(results)}`,
+  'utf8',
+);
+console.log(results);
