8993a5c59864d0d5fdee41ada91458ba1ad8dec0
jobo322 <jose.bolanos@correounivalle.edu.co>
2025-05-02 11:17:17 -0500

    chore: apply suggestion n add 1e6 factor to reach the right value or rad/s/T

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { gyromagneticRatioMHzT } from './gyromagneticRatioMHzT';
 
 export type GyromagneticNucleus = keyof typeof gyromagneticRatioMHzT;
 
-export const gyromagneticRatio: Record<GyromagneticNucleus, number> = {};
-for (const key in gyromagneticRatioMHzT) {
-  gyromagneticRatio[key] = gyromagneticRatioMHzT[key] * 2 * Math.PI;
-}
+export const gyromagneticRatio = Object.fromEntries(
+  Object.entries(gyromagneticRatioMHzT).map(([key, value]) => [
+    key,
+    value * 2 * Math.PI * 1e6,
+  ]),
+) as typeof gyromagneticRatioMHzT;
 
 export * from './gyromagneticRatioMHzT';
 export * from './getGyromagneticNucleus';
